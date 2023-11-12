93a795fbbead908b2bc90759d8d710f50b9bae10
Abdallah Hussein <abiza2013@gmail.com>
2023-11-12 20:51:17 +0200

    Handle Cases in DatabaseHandler Class

---
+++ Project/InstaPay/src/InstaPayPackage/InstapayAccount.java
@@ -0,0 +1,39 @@
+package InstaPayPackage;
+
+public class InstapayAccount {
+  private String username;
+  private String password;
+  private String numberPhone;
+  public Account accountObject;
+
+  public InstapayAccount() {
+  }
+
+  public InstapayAccount(Account account) {
+    this.accountObject = account;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setPassword(String pass) {
+    this.password = pass;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setNumberPhone(String numberP) {
+    this.numberPhone = numberP;
+  }
+
+  public String getNumberPhone() {
+    return this.numberPhone;
+  }
+}
