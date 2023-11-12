2ac2e9ba25d42e2a1d88e2306856a62604030b34
Abdallah Hussein <abiza2013@gmail.com>
2023-11-12 17:43:45 +0200

    Adding Methods in DatabaseHandler Class Creating InstaPayAccount Table

--- Project/InstaPay/src/InstaPayPackage/DatabaseHandler.java
+++ Project/InstaPay/src/InstaPayPackage/DatabaseHandler.java
@@ -1,9 +1,6 @@
 package InstaPayPackage;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseHandler {
 
@@ -26,6 +23,86 @@ public class DatabaseHandler {
       preparedStatement.executeUpdate();
     }
   }
+
+  // Method to retrieve a record from the specified table by ID
+  public void retrieveRecord(Object obj) throws SQLException {
+//    String query = "SELECT obj.balance FROM " + obj.type + " WHERE mobileNumber = ?";
+    String query = "SELECT balance FROM " + "Wallet" + " WHERE mobileNumber = ?";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+      preparedStatement.setString(1, "01112223152");
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        int retrievedBalance = resultSet.getInt("balance");
+        System.out.println("Balance: " + retrievedBalance);
+      }
+    }
+  }
+
+  public void updateRecord(Object obj) throws SQLException {
+    // Assuming obj has properties type, id, and newBalance
+//    String query = "UPDATE " + obj.type + " SET balance = ? WHERE mobileNumber = ?";
+    String query = "UPDATE " + "Wallet" + " SET balance = ? WHERE mobileNumber = ?";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+      preparedStatement.setInt(1, 666);
+      preparedStatement.setString(2, "01112223152");
+
+      preparedStatement.executeUpdate();
+    }
+  }
+
+  public boolean checkLogin(Object obj) throws SQLException {
+    //    String query = "SELECT COUNT(*) AS count FROM InstaPayAccount WHERE obj.username = ? AND obj.password = ?";
+    String query = "SELECT COUNT(*) AS count FROM InstaPayAccount WHERE username = ? AND password = ?";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+      preparedStatement.setString(1, "Abdallah");
+      preparedStatement.setString(2, "123456");
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      if (resultSet.next()) {
+        int count = resultSet.getInt("count");
+        return count == 1;
+      }
+
+      return false;
+    }
+  }
+
+  // checking existing of num or username
+//  public boolean check(Object obj) throws SQLException {
+  public boolean checkNumOrUsr(Object obj, boolean isNum) throws SQLException {
+    if (isNum) {
+      String query = "SELECT COUNT(*) AS count FROM Wallet WHERE mobileNumber = ? ";
+//    String query = "SELECT COUNT(*) AS count FROM obj.Type WHERE mobileNumber = ? ";
+      try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        preparedStatement.setString(1, "01112223152");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+          int count = resultSet.getInt("count");
+          return count == 1;
+        }
+        return false;
+      }
+    } else {
+      String query = "SELECT COUNT(*) AS count FROM InstaPayAccount WHERE username = ? ";
+      try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        preparedStatement.setString(1, "Abdallah");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+          int count = resultSet.getInt("count");
+          return count == 1;
+        }
+        return false;
+      }
+    }
+  }
+
 
   // Close the database connection
   public void closeConnection() throws SQLException {
--- Project/InstaPay/src/InstaPayPackage/Main.java
+++ Project/InstaPay/src/InstaPayPackage/Main.java
@@ -11,8 +11,11 @@ public class Main {
       databaseHandler = new DatabaseHandler();
 
       // Example: Insert records into the "wallet" table
-      databaseHandler.insertRecord(new Object());
-
+//      databaseHandler.insertRecord(new Object());
+      databaseHandler.retrieveRecord(new Object());
+      databaseHandler.updateRecord(new Object());
+      System.out.println(databaseHandler.checkLogin(new Object()));
+      System.out.println(databaseHandler.checkNumOrUsr(new Object(),false));
       // Other operations can be performed using methods in DatabaseHandler class
 
     } catch (SQLException e) {
